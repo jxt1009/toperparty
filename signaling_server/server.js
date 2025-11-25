2e25189517c78b242daba5b24867f280066eabd8
Jameson Toper <jxt1009@rit.edu>
2025-11-24 18:40:40 -0800

    updating nginx config

--- signaling_server/server.js
+++ signaling_server/server.js
@@ -1,14 +1,19 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 4001, host: "127.0.0.1"});
+const wss = new WebSocketServer({ port: 4001, host: '0.0.0.0' });
 
 wss.on('connection', (ws) => {
+  console.log('Client connected');
+
   ws.on('message', (msg) => {
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msg);
+    console.log('Received:', msg.toString());
+    // Broadcast to all clients except sender
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === client.OPEN) {
+        client.send(msg.toString());
       }
     });
   });
-});
 
+  ws.on('close', () => console.log('Client disconnected'));
+});
